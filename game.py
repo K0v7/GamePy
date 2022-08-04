1cb9bea4f4b24f523f2a985d3ccc6f53009690f4
k0v7 <contato.felipesilva2020@hotmail.com>
2022-08-04 15:27:56 -0300

    init commit

---
+++ game.py
@@ -0,0 +1,10 @@
+from models.calcular import Calcular
+
+def mains() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+    def jogar(pontos: int) -> None:
+        pass
+    if __name__ == '___main_':
+        main()
